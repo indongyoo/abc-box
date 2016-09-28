3b3dab09b73a7cff53c1a5c9016ced2a573c41f3
Cojamm <idy@marketpress.co.kr>
2016-09-28 13:55:54 +0900

    cc

--- abc.box.js
+++ abc.box.js
@@ -12,6 +12,7 @@
 !function (root, cLambda, makeConstructorBox) {
   root.Box = makeConstructorBox(root, cLambda);
 }(typeof global == 'object' && global.global == global && (global.G = global) || window, function (C) {
+  /* C.lambda forked raganwald/string-lambdas */
   return C.lambda = function (str) {
     if (typeof str !== 'string') return str;
     var expr, leftSection, params, rightSection, sections, v, vars, _i, _len;
